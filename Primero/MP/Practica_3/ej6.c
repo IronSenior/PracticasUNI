a3daaff5c1edd7473584780ed2e7403790257754
pepemarquezof <pepemarquezof@gmail.com>
2018-05-03 22:03:48 +0200

    3-05-2018

--- Primero/MP/Practica_3/ej6.c
+++ Primero/MP/Practica_3/ej6.c
@@ -1,13 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
-int main(int argc, char const *argv[])
-{
-	FILE* entrada;
-	if ((entrada = fopen(fichero, "r"))==NULL){
-		printf("Error al abrir el archivo\n");
+
+void convertir_en_mayus(FILE* entrada){
+	//He limitado ha 50 el tamaño de cada linea
+	char linea[50];
+	FILE* salida;
+	if ((salida = fopen("mayusculas.txt", "w"))==NULL){ //Debe guardarlo con combre Mayusculas_nombredelfichero.txt
+		printf("Error al crear el archivo\n");
 		exit(-1);
 	}
+
+	//Va linea por linea y letra por letra convirtiendo en mayuscula
+	while(fgets(linea, 50, entrada)!= NULL){
+		for(int i=0; i<50; i++){
+			linea[i] = toupper(linea[i]);
+		}
+		fprintf(salida, linea);
+	}
+	fclose(salida);
+}
+
+int main(int argc, char const *argv[]){
+
+	//El usuario introducirá el nombre del fichero
+	char* fichero; 
+	printf("Introduzca el nombre del fichero\n");
+	scanf("%s", fichero);
+
+	//Abrimos el fichero de texto
+	FILE* entrada;
+	if ((entrada = fopen(fichero, "r"))==NULL){
+		printf("Error al cagarse el archivo\n");
+		exit(-1);
+	}
+
+	//Convierte en mayusculas el contenido del fichero
+	convertir_en_mayus(entrada);
+	printf("Su archivo ha sido creado\n");
+
+	fclose(entrada);
 
 	
 	return 0;
