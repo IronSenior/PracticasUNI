2c3f3de6fbab20c497860eff1fd2a1544e0f876a
Pepe Marquez <pepemarquezof@gmail.com>
2018-10-07 17:08:15 +0200

    veamos

--- Segundo/POO/Practica 1/Ej3/funciones.h
+++ Segundo/POO/Practica 1/Ej3/funciones.h
@@ -1,11 +1,14 @@
 //funciones.h
 //Funciones necesarias para el juego
+#include "dados.h"
 
-#ifndef FUNCINOES_H
-#define FUNCINOES_H
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
 
+//Funcion para la accion del juego
 void accion_func(int eleccion, Dados d);
 
+//Funcion para el menu
 int menu_func(Dados d);
 
 #endif
