8acd26e91ee1e3013bfe690d6b30b16bcf56c609
Pepe Marquez <pepemarquezof@gmail.com>
2018-10-12 13:51:08 +0200

    inline

--- Segundo/POO/Practica 2/persona.h
+++ Segundo/POO/Practica 2/persona.h
@@ -25,25 +25,25 @@ class Persona{
         string new_province="", string new_country="");
 
         //Modifiers
-        void setDNI(string new_dni){dni_ = new_dni;};
-        void setNombre(string new_name){nombre_ = new_name;};
-        void setApellido(string new_lname){apellidos_ = new_lname;};
+        inline void setDNI(string new_dni){dni_ = new_dni;};
+        inline void setNombre(string new_name){nombre_ = new_name;};
+        inline void setApellido(string new_lname){apellidos_ = new_lname;};
         bool setEdad(int new_age);
-        void setDireccion(string new_adress){direccion_ = new_adress;};
-        void setLocalidad(string new_location){localidad_ = new_location;};
-        void setProvincia(string new_province){provincia_ = new_province;};
-        void setPais(string new_country){pais_ = new_country;};
+        inline void setDireccion(string new_adress){direccion_ = new_adress;};
+        inline void setLocalidad(string new_location){localidad_ = new_location;};
+        inline void setProvincia(string new_province){provincia_ = new_province;};
+        inline void setPais(string new_country){pais_ = new_country;};
 
         //Observers
-        string getDNI(){return dni_;};
-        string getNombre(){return nombre_;};
-        string getApellidos(){return apellidos_;};
+        inline string getDNI(){return dni_;};
+        inline string getNombre(){return nombre_;};
+        inline string getApellidos(){return apellidos_;};
         string getApellidosyNombre();
-        int getEdad(){return edad_;};
-        string getDireccion(){return direccion_;};
-        string getLocalidad(){return localidad_;};
-        string getProvincia(){return provincia_;};
-        string getPais(){return pais_;};
+        inline int getEdad(){return edad_;};
+        inline string getDireccion(){return direccion_;};
+        inline string getLocalidad(){return localidad_;};
+        inline string getProvincia(){return provincia_;};
+        inline string getPais(){return pais_;};
 
         //Otras funciones
         bool mayor();
