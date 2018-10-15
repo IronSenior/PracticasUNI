1b41b164c7a0e76b01ac23a9e8a732d062f0f501
Jose Marquez <pepemarquezof@gmail.com>
2018-10-15 12:36:01 +0200

    Terminada cabecera de jugador

--- Segundo/POO/Practica 2/jugador.h
+++ Segundo/POO/Practica 2/jugador.h
@@ -6,15 +6,39 @@
 
 #include "persona.h"
 #include <string>
+#include <list>
 
 using std::string;
+using std::list;
+
+struct apuesta{
+    int type;
+    string value;
+    int amount;
+};
 
 class Jugador : public Persona{
     private:
-        int dinero_;
+        string codigo_;
+        int dinero_ = 1000;
+        list<apuesta> apuestas_;
 
     public:
-        Jugador
+        Jugador(string new_dni, string new_code, string new_name="", string new_lname="", 
+        int new_age=0, string new_adress="", string new_location="", string new_province="", 
+        string new_country=""):Persona(new_dni, new_name, new_lname, 
+        new_age, new_adress, new_location, new_province, 
+        new_country){codigo_ = new_code;};
+
+        inline string const getCodigo(){return codigo_;};
+        inline int const getDinero(){return dinero_;};
+        inline list<apuesta> const getApuestas(){return apuestas_;};
+
+        inline bool setDinero(int new_money){dinero_ = new_money;};
+        inline bool setCodigo(int new_code){codigo_ = new_code;};
+
+        bool setApuestas(list<apuesta> new_apuesta);
+
 };
 
 #endif
