a41dff871f57c490bfe6ca8a482d435a06b9eaa6
Pepe Marquez <pepemarquezof@gmail.com>
2018-11-01 15:38:48 +0100

    Añadida la lista de 5 ultimos (no se ha probado)

--- Segundo/POO/Practica 1/Ej5/dados.h
+++ Segundo/POO/Practica 1/Ej5/dados.h
@@ -4,6 +4,10 @@
 #ifndef DADOS_H
 #define DADOS_H
 
+#include <list>
+using std::list;
+
+
 class Dados{
 	private:
 		int d1_;
@@ -12,23 +16,29 @@ class Dados{
 		int lanzdado2_;
 		int d1Suma_;
 		int d2Suma_;
-		int vultimos1_[5];
-		int vultimos2_[5];
+		list<int> vultimos1_;
+		list<int> vultimos2_;
+		void savelanzamiento1_(int lanz);
+		void savelanzamiento2_(int lanz);
 
 	public:
 		Dados();
 		void lanzamiento();
-		int getDado1();
-		int getDado2();
+		inline int getDado1() const {return d2_;};
+		inline int getDado2() const {return d2_;};
 		bool setDado1(int valor);
 		bool setDado2(int valor);
 		int getSuma();
-		int getLanzamientos1();
-		int getLanzamientos2();
+		inline int getLanzamientos1() const {return lanzdado1_;};
+		inline int getLanzamientos2() const {return lanzdado2_;};
 		float getMedia1();
 		float getMedia2();
-		int getVultimos1();
-		int getVultimos2();
+
+		//Devuelven la lista de los 5 ultimos lanzamientos
+		//El último elemento de la lista es el último lanzamiento
+		//No cuenta como lanzamiento el setDadox
+		inline list<int> getVultimos1() const {return vultimos1_;};
+		inline list<int> getVultimos2() const {return vultimos1_;};
 };
 
 #endif
