a1f84b2802b9e30f94464da037175f5db216da82
Jose Marquez <pepemarquezof@gmail.com>
2018-11-07 13:37:16 +0100

    Ejercicio 2 Pracica 4

---
+++ Segundo/BD/Practica 4/Ejercicio_2.sql
@@ -0,0 +1,8 @@
+set serveroutput on;
+DECLARE
+  mydni NUMBER := 30983712;
+  votante votantes%rowtype;
+BEGIN
+  SELECT * INTO votante FROM votantes WHERE dni = mydni;
+  dbms_output.put_line( SUBSTR(votante.nombrecompleto,1,INSTR(votante.nombrecompleto,' '))||'con correo '||votante.email);
+END;
