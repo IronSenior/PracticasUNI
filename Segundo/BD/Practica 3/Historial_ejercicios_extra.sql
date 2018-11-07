810ccb3a6a617927c04cb64db871c342cab6ed66
Jose Marquez <pepemarquezof@gmail.com>
2018-11-07 13:14:45 +0100

    Ejercicio extra 2

--- Segundo/BD/Practica 3/Historial_ejercicios_extra.sql
+++ Segundo/BD/Practica 3/Historial_ejercicios_extra.sql
@@ -2,3 +2,14 @@
 SELECT CONCAT(CONCAT(l.nombre, ' va antes que '), lc.nombre) FROM localidades l, localidades lc
 WHERE l.idlocalidad + 1 = lc.idlocalidad
 ORDER BY l.idlocalidad;
+
+--Ejercicio 2
+SELECT nombre FROM localidades
+  WHERE numerohabitantes > (
+    SELECT numerohabitantes FROM localidades
+      WHERE idlocalidad = (
+      SELECT localidad FROM votantes
+        WHERE fechanacimiento = (
+          SELECT MIN(fechanacimiento) FROM votantes
+            WHERE fechanacimiento > (
+              SELECT MIN(fechanacimiento) from votantes))));
