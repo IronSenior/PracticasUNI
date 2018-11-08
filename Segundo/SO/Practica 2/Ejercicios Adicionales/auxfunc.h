a37bd389c461a97d02d07cfb744f0be2e2579893
Jose Marquez <pepemarquezof@gmail.com>
2018-11-08 10:20:07 +0100

    Cabecera de auxiliares

---
+++ Segundo/SO/Practica 2/Ejercicios Adicionales/auxfunc.h
@@ -0,0 +1,20 @@
+//Fichero de cabecera de funciones auxialiares
+//José Márquez Doblas (GG2)
+
+#ifndef AUXFUNC_H
+#define AUXFUNC_H
+
+
+#define MATRIXI 3
+#define MATRIXJ 3
+#define VECTOR  3
+
+void get_matrix(int** matrix);
+
+void get_vector(int* vector);
+
+void multiplyBase(int* vector, int** matrix, int* result);
+
+void print_result(int* result);
+
+#endif
