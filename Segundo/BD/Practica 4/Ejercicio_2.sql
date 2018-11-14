45bcd6d38e3a725e215a9a1e42d49aa16f1593fd
Jose Marquez <pepemarquezof@gmail.com>
2018-11-14 13:05:21 +0100

    cambios menores

--- Segundo/BD/Practica 4/Ejercicio_2.sql
+++ Segundo/BD/Practica 4/Ejercicio_2.sql
@@ -6,3 +6,4 @@ BEGIN
   SELECT * INTO votante FROM votantes WHERE dni = mydni;
   dbms_output.put_line( SUBSTR(votante.nombrecompleto,1,INSTR(votante.nombrecompleto,' '))||'con correo '||votante.email);
 END;
+
