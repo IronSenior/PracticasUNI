9612d9225dbe0ea71cd42ba6dc2b69e4006e6f8c
Jose Marquez <pepemarquezof@gmail.com>
2018-11-29 09:57:55 +0100

    Ejercicio 3 SO parte 2 casi terminada

--- Segundo/SO/Practica 2/ej3_2.c
+++ Segundo/SO/Practica 2/ej3_2.c
@@ -5,10 +5,14 @@
 #include <stdio.h>
 
 #define V   5   //Buffer
-#define P   2   //Number of producers
-#define C   2   //Number of consumers
+#define P   1   //Number of producers
+#define C   3   //Number of consumers
 
 int buffer[V];
+int suma_productores = 0;
+int suma_consumidores = 0;
+int indice_productores = -1;
+int indice_consumidores = -1;
 sem_t empty, full, mutex;
 
 
@@ -26,6 +30,8 @@ int main(int argc, char const *argv[]){
     //Returns values
     int *r_producer_value;
     int *r_consumer_value;
+    extern int suma_consumidores;
+    extern int suma_productores;
 
     srand(time(NULL));
 
@@ -45,12 +51,19 @@ int main(int argc, char const *argv[]){
         if ((status = pthread_create(&consumers[i], NULL, consumer, (void *) &i)))
             exit(status);
     }
-    //Waiting for threads
-    pthread_join(producers, (void **) &r_producer_value);
-    pthread_join(consumers, (void **) &r_consumer_value);
 
-    printf("Valor suma del productor %i \n", *r_producer_value);
-    printf("Valor suma del consumidor %i \n", *r_consumer_value);
+    //Waiting for threads
+    for(i=0; i<P; i++){
+        pthread_join(producers[i], (void **) &r_producer_value);
+        printf("Value returned by producer %lu: %d \n", producers[i], *r_producer_value);
+    }
+    for(i=0; i<C; i++){
+        pthread_join(consumers[i], (void **) &r_consumer_value);
+        printf("Value returned by consumer %lu: %d \n", consumers[i], *r_consumer_value);
+    }
+
+    printf("Suma productores: %d\n", suma_productores);
+    printf("Suma consumidores: %d\n", suma_consumidores);
 
     //Destoy Semaphores
     sem_destroy(&mutex);
@@ -64,9 +77,10 @@ void *producer(void *p){
 
     //Variable declaration
     int i, dato, puntero;
-    int suma = 0;
     int *to_return;
     extern int buffer[V];
+    extern int suma_productores;
+    extern int indice_productores;
     extern sem_t empty, full, mutex;
 
 
@@ -74,14 +88,14 @@ void *producer(void *p){
         dato = rand() % 1001; //generate data
         sem_wait(&empty);
         sem_wait(&mutex);
-        puntero = i%5;  
-        buffer[puntero] = dato;
+        indice_productores = (indice_productores+1)%V;
+        buffer[indice_productores] = dato;
+        suma_productores += dato;
         sem_post(&mutex);
         sem_post(&full);
-        suma = suma + dato;
     }
     to_return = malloc(sizeof(int));
-    *to_return = suma;
+    *to_return = suma_productores;
 
     pthread_exit((void *) to_return);
 }
@@ -89,7 +103,8 @@ void *producer(void *p){
 void *consumer(void *p){
 
     int i, dato, puntero;
-    int suma = 0;
+    extern int suma_consumidores;
+    extern int indice_consumidores;
     int *to_return;
     extern int buffer[V];
     extern sem_t empty, full, mutex;
@@ -97,17 +112,17 @@ void *consumer(void *p){
     for(i=0; i<1000; i++){
         sem_wait(&full);
         sem_wait(&mutex);
-        puntero = i%5;
-        dato =  buffer[puntero];
+        indice_consumidores = (indice_consumidores+1)%V;
+        dato =  buffer[indice_consumidores];
+        suma_consumidores += dato;
         sem_post(&mutex);
         sem_post(&empty);
-        suma = suma + dato;
     }
 
     //Para hacer el return de los hilos hay que reservar memoria
     to_return = malloc(sizeof(int));
 
-    *to_return = suma;
+    *to_return = suma_consumidores;
 
     pthread_exit((void *) to_return);
 }
