14d3274f4ab0c20f2e74742174d812ebf96dc248
Pepe <pepemarquezof@gmail.com>
2019-05-21 12:39:00 +0200

    Empezando la practica 4 de PAS

---
+++ Segundo/PAS/Practica 4/ejercicio1.sh
@@ -0,0 +1,42 @@
+# ARG1 Fichero
+
+echo '********* -- 1'
+echo "Mostrar duración de las películas"
+grep -E '... ..min\>' $1
+
+echo '********* -- 2'
+echo "Mostrar la linea del pais de la película"
+grep -E '\<\-[a-z]*\>' $1
+
+echo '********* -- 3'
+echo "Mostrar solamente el pais de la película"
+grep -E '\<\-[a-z]\>' $1
+
+echo '********* -- 4'
+echo "Contar cuantas peiculas son del 2016 y cuantas del 2017"
+echo "Hay $(grep -E -c '../../2016' $1) peliculas de 2016 y $(grep -E -c '../../2017' $1) de 2017"
+
+echo '********* -- 5'
+echo "Mostrar todo el documento pero sin lineas vacias"
+grep -v -E '^\s*$' $1
+
+echo '********* -- 6'
+echo "Mostrar todas las lineas que empiezan por E haya o espacios delante"
+grep -E '^ *E' $1
+
+echo '********* -- 7'
+echo "Lineas que contienen l t o d con vocales en medio"
+grep -E -e '[a-z]*t[a-u]t[a-z]*' -e '[a-z]*d[a-u]d[a-z]*' -e '[a-z]*l[a-u]l[a-z]*' $1
+
+echo '********* -- 8'
+echo "Lineas que tienen 6 o más aes"
+grep -E '(.*a.*){6,}' $1
+
+echo '********* -- 9'
+echo "Lineas que terminan en ... y no empiezan por espacio"
+grep -E '^[^ ].*/\.\.\./$' $1
+
+echo "********* -- 10"
+
+
+
