bd912b9feca49c77ac0d556fd6629df99705b565
Pepe <pepemarquezof@gmail.com>
2019-10-10 16:44:33 +0200

    Test para la clase DominoBoard

--- Tercero/Redes/Practica 2/test/test_DominoBoard.cpp
+++ Tercero/Redes/Practica 2/test/test_DominoBoard.cpp
@@ -18,7 +18,7 @@ TEST_CASE("Putting two start tokens")
 
 }
 
-TEST_CASE("Putting the second token")
+TEST_CASE("Putting the second token con left")
 {
     DominoBoard TestingBoard;
     DominoToken StartToken(6, 6);
@@ -38,4 +38,27 @@ TEST_CASE("Putting the second token")
     TestingBoard.PutTokenOnLeft(SecondToken);
     CHECK(TestingBoard.CanPutOnLeft(ThirdToken));
     CHECK_FALSE(TestingBoard.CanPutOnRight(ThirdToken));
+}
+
+
+TEST_CASE("Putting the second token con Right")
+{
+    DominoBoard TestingBoard;
+    DominoToken StartToken(6, 6);
+
+    DominoToken SecondToken(6, 4);
+    DominoToken ThirdToken(4, 5);
+    
+
+    TestingBoard.PutFirstToken(StartToken);
+
+    CHECK(TestingBoard.CanPutOnLeft(SecondToken));
+    CHECK_FALSE(TestingBoard.CanPutOnLeft(ThirdToken));
+
+    CHECK(TestingBoard.CanPutOnRight(SecondToken));
+    CHECK_FALSE(TestingBoard.CanPutOnRight(ThirdToken));
+
+    TestingBoard.PutTokenOnRight(SecondToken);
+    CHECK(TestingBoard.CanPutOnRight(ThirdToken));
+    CHECK_FALSE(TestingBoard.CanPutOnLeft(ThirdToken));
 }
