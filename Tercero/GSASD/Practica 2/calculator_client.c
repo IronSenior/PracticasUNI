b14c24057f75776c0a396b3a50882aea8bdd4a09
Pepe Marquez <pepemarquezof@gmail.com>
2019-11-24 17:12:55 +0100

    Calculadora hecha

--- Tercero/GSASD/Practica 2/calculator_client.c
+++ Tercero/GSASD/Practica 2/calculator_client.c
@@ -6,60 +6,75 @@
 
 #include "calculator.h"
 
-
-void
-calculadora_prog_1(char *host)
+int
+calculadora_prog_1(char *host, char *operation, struct operandos operands)
 {
 	CLIENT *clnt;
 	int  *result_1;
-	operandos  suma_1_arg;
-	int  *result_2;
-	operandos  resta_1_arg;
-	int  *result_3;
-	operandos  multiplicacion_1_arg;
-	int  *result_4;
-	operandos  division_1_arg;
 
-#ifndef	DEBUG
-	clnt = clnt_create (host, CALCULADORA_PROG, CALCULADORA_VERS, "udp");
-	if (clnt == NULL) {
-		clnt_pcreateerror (host);
-		exit (1);
-	}
-#endif	/* DEBUG */
+	#ifndef	DEBUG
+		clnt = clnt_create (host, CALCULADORA_PROG, CALCULADORA_VERS, "udp");
+		if (clnt == NULL) {
+			clnt_pcreateerror (host);
+			exit (1);
+		}
+	#endif	/* DEBUG */
 
-	result_1 = suma_1(&suma_1_arg, clnt);
-	if (result_1 == (int *) NULL) {
-		clnt_perror (clnt, "call failed");
+	if(strcmp(operation, "SUMA") == 0){
+		result_1 = suma_1(&operands, clnt);
+		if (result_1 == (int *) NULL) {
+			clnt_perror (clnt, "call failed");
+		}
 	}
-	result_2 = resta_1(&resta_1_arg, clnt);
-	if (result_2 == (int *) NULL) {
-		clnt_perror (clnt, "call failed");
+	if(strcmp(operation, "RESTA") == 0){
+		result_1 = resta_1(&operands, clnt);
+		if (result_1 == (int *) NULL) {
+			clnt_perror (clnt, "call failed");
+		}
 	}
-	result_3 = multiplicacion_1(&multiplicacion_1_arg, clnt);
-	if (result_3 == (int *) NULL) {
-		clnt_perror (clnt, "call failed");
+	if(strcmp(operation, "MULTIPLICACION") == 0){
+		result_1 = multiplicacion_1(&operands, clnt);
+		if (result_1 == (int *) NULL) {
+			clnt_perror (clnt, "call failed");
+		}
 	}
-	result_4 = division_1(&division_1_arg, clnt);
-	if (result_4 == (int *) NULL) {
-		clnt_perror (clnt, "call failed");
+	if(strcmp(operation, "DIVISION") == 0){
+		result_1 = division_1(&operands, clnt);
+		if (result_1 == (int *) NULL) {
+			clnt_perror (clnt, "call failed");
+		}
 	}
-#ifndef	DEBUG
-	clnt_destroy (clnt);
-#endif	 /* DEBUG */
+
+	#ifndef	DEBUG
+		clnt_destroy (clnt);
+	#endif	 /* DEBUG */
+
+	return *result_1;
 }
 
 
-int
-main (int argc, char *argv[])
+int main (int argc, char *argv[])
 {
 	char *host;
+	struct operandos operands;
+	char operation[10];
 
 	if (argc < 2) {
 		printf ("usage: %s server_host\n", argv[0]);
 		exit (1);
 	}
+
+	printf("Introduzca el primer operando: ");
+	scanf("%i", &operands.a);
+	printf("Introduzca el segundo operando: ");
+	scanf("%i", &operands.b);
+	printf("Introduzca la operacion: ");
+	scanf("%s", operation);
+
 	host = argv[1];
-	calculadora_prog_1 (host);
-exit (0);
+	int result = calculadora_prog_1 (host, operation, operands);
+
+	printf("El resultado es: %i", result);
+
+	exit (0);
 }
